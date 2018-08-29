a4babee7623112dfb94d9b0cb93b30b120a91f25
Jacob Barrow <barrow.jacob@gmail.com>
2018-08-29 14:05:54 +0100

    sticked footer to bottom

--- partials/footer.php
+++ partials/footer.php
@@ -1,14 +1,14 @@
 <div id="footer" role="contentinfo">
-    <div class="left">
+    <div class="sponsors">
         Proudly sponsored by:
-        <div class="sponsors">
+        <div>
             <img src="http://via.placeholder.com/120x80" alt="logo">
             <img src="http://via.placeholder.com/120x80" alt="logo">
             <img src="http://via.placeholder.com/120x80" alt="logo">
             <img src="http://via.placeholder.com/120x80" alt="logo">
         </div>
     </div>
-    <div class="right">
+    <div class="social">
         <a href="#">contact@enucs.org.uk</a>
         <a href="#">committee@enucs.org.uk</a>
         <div class="social">
