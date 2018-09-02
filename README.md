0dd51cabadf636180d8529c44a0d9995232aa59e
Jacob Barrow <barrow.jacob@gmail.com>
2018-09-02 15:19:14 +0100

    stripped .php extension

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This is the code that's behind our website, enucs.org.uk. It's not overly compli
 
 I don't know why you'd want to run this for yourself, it's just a website, but if you do just stick it on a PHP enabled server, or use the command `php -S localhost:8000` from the project root.
 
+Note that the nav probably won't work with a local instance, as on the server we strip the `.php` extension
+
 Compile sass with `sass --watch sass/main.sass:css/main.css`.
 
 ## License
--- partials/nav.php
+++ partials/nav.php
@@ -1,12 +1,12 @@
 <div id="nav" role="navigation">
     <ul id="desktop">
         <li><a href="/" <?= $page == 'events' ? 'class="active"' : '' ?>>events</a></li>
-        <li><a href="/society.php" <?= $page == 'society' ? 'class="active"' : '' ?>>society</a></li>
+        <li><a href="/society" <?= $page == 'society' ? 'class="active"' : '' ?>>society</a></li>
         <li class="logo">
             <span class="logo dark">{<span class="enu">enu</span><span class="cs">cs</span>}</span>
         </li>
-        <li><a href="/sponsors.php" <?= $page == 'sponsors' ? 'class="active"' : '' ?>>sponsors</a></li>
-        <li><a href="/join.php" <?= $page == 'join' ? 'class="active"' : '' ?>>join us</a></li>
+        <li><a href="/sponsors" <?= $page == 'sponsors' ? 'class="active"' : '' ?>>sponsors</a></li>
+        <li><a href="/join" <?= $page == 'join' ? 'class="active"' : '' ?>>join us</a></li>
         <li id="hamburger" onclick="toggleNav()">
             <i class="fa fa-bars"></i>
         </li>
@@ -14,8 +14,8 @@
 
     <ul id="mobile" class="disabled" aria-hidden="true">
         <li><a href="/" <?= $page == 'events' ? 'class="active"' : '' ?>>events</a></li>
-        <li><a href="/society.php" <?= $page == 'society' ? 'class="active"' : '' ?>>society</a></li>
-        <li><a href="/sponsors.php" <?= $page == 'sponsors' ? 'class="active"' : '' ?>>sponsors</a></li>
-        <li><a href="/join.php" <?= $page == 'join' ? 'class="active"' : '' ?>>join us</a></li>
+        <li><a href="/society" <?= $page == 'society' ? 'class="active"' : '' ?>>society</a></li>
+        <li><a href="/sponsors" <?= $page == 'sponsors' ? 'class="active"' : '' ?>>sponsors</a></li>
+        <li><a href="/join" <?= $page == 'join' ? 'class="active"' : '' ?>>join us</a></li>
     </ul>
 </div>
