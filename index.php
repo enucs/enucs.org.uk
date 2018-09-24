f4f4ee2e715593bde1d8216548a84c3aceb814da
Jacob Barrow <barrow.jacob@gmail.com>
2018-09-24 15:59:11 +0100

    Added events

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    $page = 'events';
+?>
+<html lang="en">
+    <?php include 'partials/head.php' ?>
+    <body>
+        <?php include 'partials/nav.php' ?>
+        <div id="content" role="main">
+            <div class="event">
+                <div class="date">26/09/18</div>
+                <div class="details">
+                    <h2>Hackathon</h2>
+                    <span class="info">17:00 | Merchiston, D2</span>
+                    <p>Join us for our first hackathon of the year, where you can work in a team to build a project on a particular theme or to solve a problem. Free entry to members and non-members alike.</p>
+                </div>
+                <div class="cf"></div>
+            </div>
+            <div class="event">
+                <div class="date">03/10/18</div>
+                <div class="details">
+                    <h2>Hackathon Review</h2>
+                    <span class="info">17:00 | Merchiston, D2</span>
+                    <p>We'll review the team's projects and award a prize to the winner. If you didn't join in with the hackathon, you can still come along - there'll most likely be free pizza.</p>
+                </div>
+                <div class="cf"></div>
+            </div>
+            <div class="event">
+                <div class="date">10/10/18</div>
+                <div class="details">
+                    <h2>PC Speed Build</h2>
+                    <span class="info">17:00 | Merchiston, D2</span>
+                    <p>Build a computer as fast as you can, the winner will get a prize!</p>
+                </div>
+                <div class="cf"></div>
+            </div>
+            <div class="event">
+                <div class="date">17/10/18</div>
+                <div class="details">
+                    <h2>Web Dev workshop</h2>
+                    <span class="info">17:00 | Merchiston, D2</span>
+                    <p>Details to be confirmed, and more events being planned - stay tuned!</p>
+                </div>
+                <div class="cf"></div>
+            </div>
+        </div>
+        <?php include 'partials/footer.php' ?>
+        <?php include 'partials/scripts.php' ?>
+    </body>
+</html>
