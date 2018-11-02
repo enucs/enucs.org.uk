d6d844f2463349cd5053582318ee6d8e0a9531d8
glugg23 <m.leonhardt424@gmail.com>
2018-11-02 11:28:40 +0000

    updated event list

--- index.php
+++ index.php
@@ -7,29 +7,11 @@
         <?php include 'partials/nav.php' ?>
         <div id="content" role="main">
             <div class="event">
-                <div class="date">17/10/18</div>
+                <div class="date">7/11/18</div>
                 <div class="details">
-                    <h2>Web Dev and Assembly Workshop</h2>
-                    <span class="info">17:00 | Merchiston, D2</span>
-                    <p>You can come learn about either HTML and CSS, or ARM assembly if you really don't like web dev. For those who already know some web dev we're also going to be covering the PHP framework Laravel.</p>
-                </div>
-                <div class="cf"></div>
-            </div>
-            <div class="event">
-                <div class="date">24/10/18</div>
-                <div class="details">
-                    <h2>Web Dev And Assembly Workshop Continued</h2>
-                    <span class="info">17:00 | Merchiston, D2</span>
-                    <p>Due to popular demand, we are continuing our workshop for another week!</p>
-                </div>
-                <div class="cf"></div>
-            </div>
-            <div class="event">
-                <div class="date">31/10/18</div>
-                <div class="details">
-                    <h2>An Evening with the British Computer Society</h2>
-                    <span class="info">17:00 - 19:00 | Merchiston, B2</span>
-                    <p>We're hosting a talk from the British Computer Society about careers in the IT industry. <a href="https://www.eventbrite.co.uk/e/an-evening-with-the-british-computer-society-tickets-51331906113">Click here for more info</a></p>
+                    <h2>ENUCS Social - Laser Tag</h2>
+                    <span class="info">17:30 - 19:00 | Laser Station, Fountain Park</span>
+                    <p>Our first social event! Members and non-members can come and join us play laser tag at Fountain Park. <a href="https://www.eventbrite.co.uk/e/enucs-social-laser-tag-laser-station-fountain-park-tickets-52145035204">Click here to get a ticket</a></p>
                 </div>
                 <div class="cf"></div>
             </div>
