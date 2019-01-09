9bee1e17a167de3cca6a978eb14acfe8c4107ed6
glugg23 <m.leonhardt424@gmail.com>
2019-01-09 20:19:04 +0000

    update events

--- index.php
+++ index.php
@@ -25,11 +25,29 @@
                 <div class="cf"></div>
             </div>
             <div class="event">
+                <div class="date">23/01/19</div>
+                <div class="details">
+                    <h2>PC Speed Build</h2>
+                    <span class="info">17:00 - 18:00 | Merchiston, D2</span>
+                    <p>Learn about hardware by building a computer as fast as possible.</p>
+                </div>
+                <div class="cf"></div>
+            </div>
+            <div class="event">
                 <div class="date">25/01/19</div>
                 <div class="details">
                     <h2>Global Game Jam</h2>
                     <span class="info">TBC | Merchiston, JKCC</span>
                     <p>We'll be joining in the Global Game Jam as an ENUCS team. <a href="https://globalgamejam.org/2019/jam-sites/edinburgh-global-game-jam">Register here</a></p>
+                </div>
+                <div class="cf"></div>
+            </div>
+            <div class="event">
+                <div class="date">30/01/19</div>
+                <div class="details">
+                    <h2>GIT Good</h2>
+                    <span class="info">17:00 - 18:00 | Merchiston, D2</span>
+                    <p>A chance to learn about git, the most widely used version control system in the industry.</p>
                 </div>
                 <div class="cf"></div>
             </div>
@@ -52,11 +70,38 @@
                 <div class="cf"></div>
             </div>
             <div class="event">
+                <div class="date">13/02/19</div>
+                <div class="details">
+                    <h2>Careers Panel Event</h2>
+                    <span class="info">TBC | Merchiston</span>
+                    <p>A panel of employers who will speak about their experiences in the industry, followed by a Q&A session.</p>
+                </div>
+                <div class="cf"></div>
+            </div>
+            <div class="event">
                 <div class="date">20/02/19</div>
                 <div class="details">
                     <h2>Competitive Programming Event</h2>
                     <span class="info">17:00 - 19:00 | Merchiston, D2</span>
                     <p>Try to solve a series of puzzles and challenges as quickly as you can using programming.</p>
+                </div>
+                <div class="cf"></div>
+            </div>
+            <div class="event">
+                <div class="date">23/02/19</div>
+                <div class="details">
+                    <h2>RGUHack!</h2>
+                    <span class="info">TBC | Robert Gorden University, Aberdeen</span>
+                    <p>Hackathon at Robert Gorden University. <a href="https://rguhack.uk/">Register here</a></p>
+                </div>
+                <div class="cf"></div>
+            </div>
+            <div class="event">
+                <div class="date">27/02/19</div>
+                <div class="details">
+                    <h2>Project Showcase</h2>
+                    <span class="info">17:00 - 18:00 | Merchiston, D2</span>
+                    <p>A chance to showoff any projects you might be working on.</p>
                 </div>
                 <div class="cf"></div>
             </div>
